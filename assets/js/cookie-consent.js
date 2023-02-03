d73c69a89f7bc9e93b1151f23b42561538a79702
Sam Hartharn-Evans <57717460+HartharnSam@users.noreply.github.com>
2023-02-03 12:20:27 +0000

    Create cookie-consent.js

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,13 @@
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "type": "opt-in"
+});
+</script>
