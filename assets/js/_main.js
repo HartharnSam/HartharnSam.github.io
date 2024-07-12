c1fabc95b7a96e48b415dfff6eb6a99bd7ccd503
Sam Hartharn-Evans <s.hartharn-evans2@ncl.ac.uk>
2024-07-12 14:39:10 +0100

    Fix to _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -134,14 +134,15 @@ $(function() {
     }
   });
 });
+
 console.log("Loaded _main.js");
-
 function loadGAonConsent() {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'G-BFQ2RTGPPB', 'auto');
-  ga('send', 'pageview');
+	var script = document.createElement('script');
+	script.async = true;
+	script.src = 'https://www.googletagmanager.com/gtag/js?id=G-BF12RTGPPB';
+	document.head.appendChild(script);
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){datalayer.push(arguments);}
+	gtag('js', new Date()));
+	gtag('config', 'G-BFQ2RTGPPB');
 }
