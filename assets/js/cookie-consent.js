f20ec6346b4f520e85546ae302f783969c801141
Sam Hartharn-Evans <s.hartharn-evans2@ncl.ac.uk>
2024-07-12 08:50:37 +0100

    Update cookie banner to actually work!

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -13,34 +13,43 @@ window.cookieconsent.initialise({
       "href": "https://hartharnsam.github.io/privacy"
   },
   onInitialise: function (status) {
+	console.log('Cookie consent initialised with status:', status);
     var type = this.options.type;
     var didConsent = this.hasConsented();
     if (type == 'opt-in' && didConsent) {
       // enable cookies
+	  console.log('User consented. Enabling cookies');
       loadGAonConsent();
     }
     if (type == 'opt-out' && !didConsent) {
       // disable cookies
+	  console.log('User didn't consent. Disabling cookies');
     }
   },
   onStatusChange: function(status, chosenBefore) {
+	console.log('Status changed to:', status);
     var type = this.options.type;
     var didConsent = this.hasConsented();
     if (type == 'opt-in' && didConsent) {
       // enable cookies
+	  console.log('User consented. Enabling cookies.');
       loadGAonConsent();
     }
     if (type == 'opt-out' && !didConsent) {
-      // disable cookies
+      console.log('User did not consent. Disabling cookies.');
+	  // disable cookies
     }
   },
   onRevokeChoice: function() {
+	console.log('User revoked choice.');
     var type = this.options.type;
     if (type == 'opt-in') {
+		console.log('Disabling cookies.');
       // disable cookies
     }
     if (type == 'opt-out') {
       // enable cookies
+	  console.log('Enabling cookies');
       loadGAonConsent();
     }
   }
